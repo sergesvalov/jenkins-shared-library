eb6c2a77ba07720163c533d98fd44b43f15dd3d3
Serge Svalov <wndrsw@gmail.com>
2026-08-15 15:15:23 +0300

    feat: use ansible-vault-pass credential automatically

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -1,8 +1,9 @@
-def call(Map params = [:]) {
+﻿def call(Map params = [:]) {
     def inventory = params.inventory ?: 'ansible/inventory.ini'
     def playbook = params.playbook
     def sshCredential = params.sshCredential
     def extraArgs = params.extraArgs ?: ''
+    def vaultCredential = params.vaultCredential ?: 'ansible-vault-pass'
 
     if (!playbook) {
         error("You must specify a 'playbook' parameter for runAnsible.")
@@ -12,21 +13,27 @@ def call(Map params = [:]) {
     }
 
     sshagent(credentials: [sshCredential]) {
-        sh """
-        # Устанавливаем Ansible локально в папке проекта, чтобы не зависеть от сервера Jenkins
-        if ! command -v ansible-playbook &> /dev/null; then
-            python3 -m venv ansible-venv
-            . ansible-venv/bin/activate
-            pip install ansible
-        else
-            echo "Ansible уже установлен глобально"
-        fi
-        
-        # Запускаем плейбук (используем venv если создали)
-        if [ -d "ansible-venv" ]; then
-            . ansible-venv/bin/activate
-        fi
-        ansible-playbook -i ${inventory} ${playbook} ${extraArgs}
-        """
+        withCredentials([string(credentialsId: vaultCredential, variable: 'VAULT_PASS')]) {
+            sh """
+            # Гарантируем удаление файла с паролем при любом исходе
+            trap 'rm -f .vault_pass' EXIT
+            echo "$VAULT_PASS" > .vault_pass
+
+            # Устанавливаем Ansible локально в папке проекта, чтобы не зависеть от сервера Jenkins
+            if ! command -v ansible-playbook &> /dev/null; then
+                python3 -m venv ansible-venv
+                . ansible-venv/bin/activate
+                pip install ansible
+            else
+                echo "Ansible уже установлен глобально"
+            fi
+            
+            # Запускаем плейбук (используем venv если создали)
+            if [ -d "ansible-venv" ]; then
+                . ansible-venv/bin/activate
+            fi
+            ansible-playbook -i ${inventory} ${playbook} --vault-password-file .vault_pass ${extraArgs}
+            """
+        }
     }
 }
